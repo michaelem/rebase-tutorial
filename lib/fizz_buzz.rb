17f303212a33119e0c24e011c12bb9af2ffbc711
Michael Emhofer <michael.emhofer@bcgdv.com>
2019-06-06 18:38:11 +0200

    Extract implementation

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,2 +1,17 @@
 class FizzBuzz
+  def initialize(number)
+    @number = number
+  end
+
+  def evaluate
+    if @number % 3 == 0 && @number % 5 == 0
+      "Fizz Buzz"
+    elsif @number % 3 == 0
+      "Fizz"
+    elsif @number % 5 == 0
+      "Buzz"
+    else
+      @number
+    end
+  end
 end
