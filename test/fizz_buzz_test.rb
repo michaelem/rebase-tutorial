efba2e89f0da04ce5f8704e88858a4be66f9353a
Michael Emhofer <michael.emhofer@bcgdv.com>
2019-06-06 18:38:11 +0200

    Use spaces

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,12 +1,11 @@
-require "minitest/autorun"
 require "test_helper"
 
 class FizzBuzzTest < Minitest::Test
-	def test_first_fizz
-		assert_equal "Fizz", FizzBuzz.new(3).evaluate
-	end
+  def test_first_fizz
+    assert_equal "Fizz", FizzBuzz.new(3).evaluate
+  end
 
-	def test_first_buzz
-		asssert_equal "Buzz", FizzBuzz.new(5).evaluate
-	end
+  def test_first_buzz
+    asssert_equal "Buzz", FizzBuzz.new(5).evaluate
+  end
 end
