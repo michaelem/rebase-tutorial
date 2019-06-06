12298349f4eb592eacb7a72b187cd24464e59f15
Michael Emhofer <michael.emhofer@bcgdv.com>
2019-06-06 18:37:09 +0200

    Add basic test-cases

---
+++ test/fizz_buzz_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class FizzBuzzTest < Minitest::Test
+  def test_first_fizz
+    assert_equal "Fizz", FizzBuzz.new(3).evaluate
+  end
+
+  def test_first_buzz
+    assert_equal "Buzz", FizzBuzz.new(5).evaluate
+  end
+end
